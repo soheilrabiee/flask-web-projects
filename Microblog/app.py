7c59f919de0495b3531195fbb5350a74f83ed7d9
Soheil <iamsoheil.r@gmail.com>
2024-05-22 16:22:35 +0330

    Recieve form data

--- Microblog/app.py
+++ Microblog/app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 
+# Create flask app
 app = Flask(__name__)
 
 
-@app.route("/")
+# Endpoint might recieve POST and GET requests
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        # Taking the entry content
+        entry_content = request.form.get("content")
+        # Time of creation for the taken entry
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        print(entry_content, formatted_date)
     return render_template("home.html")
