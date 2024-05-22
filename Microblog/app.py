799459d040d97138d2b1f995313bafc7e693d868
Soheil <iamsoheil.r@gmail.com>
2024-05-22 16:47:26 +0330

    Add entries template and display past entries

--- Microblog/app.py
+++ Microblog/app.py
@@ -4,6 +4,8 @@ import datetime
 # Create flask app
 app = Flask(__name__)
 
+entries = []
+
 
 # Endpoint might recieve POST and GET requests
 @app.route("/", methods=["GET", "POST"])
@@ -11,7 +13,18 @@ def home():
     if request.method == "POST":
         # Taking the entry content
         entry_content = request.form.get("content")
-        # Time of creation for the taken entry
+        # Time of creation of the taken entry for datetime in html
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template("home.html")
+        entries.append((entry_content, formatted_date))
+
+    # Formatted time for our design
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"),
+        )
+        for entry in entries
+    ]
+
+    return render_template("home.html", entries=entries_with_date)
