8d4bb252aaa767523a2515256b085b400e91ec76
Soheil <iamsoheil.r@gmail.com>
2024-05-23 01:24:11 +0330

    Store entry data into mongodb

--- Microblog/app.py
+++ Microblog/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 import datetime
 
 # Create flask app
 app = Flask(__name__)
+
+# Database config
+client = MongoClient("mongodb://localhost:27017")
+app.db = client.microblog
 
 entries = []
 
@@ -16,6 +21,9 @@ def home():
         # Time of creation of the taken entry for datetime in html
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
+
+        # Inserting data into our mongo database
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
     # Formatted time for our design
     entries_with_date = [
