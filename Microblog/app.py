da173e87f53c3097d959cb64cdb74cea1068571a
Soheil <iamsoheil.r@gmail.com>
2024-05-23 20:22:47 +0330

    Retrieve entry data from mongodb

--- Microblog/app.py
+++ Microblog/app.py
@@ -9,8 +9,6 @@ app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017")
 app.db = client.microblog
 
-entries = []
-
 
 # Endpoint might recieve POST and GET requests
 @app.route("/", methods=["GET", "POST"])
@@ -20,7 +18,6 @@ def home():
         entry_content = request.form.get("content")
         # Time of creation of the taken entry for datetime in html
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
 
         # Inserting data into our mongo database
         app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
@@ -28,11 +25,11 @@ def home():
     # Formatted time for our design
     entries_with_date = [
         (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"),
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"),
         )
-        for entry in entries
+        for entry in app.db.entries.find()
     ]
 
     return render_template("home.html", entries=entries_with_date)
