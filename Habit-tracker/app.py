529acb896b3573447dadd3d05a347acd53936037
Soheil <iamsoheil.r@gmail.com>
2024-05-25 23:58:39 +0330

    Display habits

--- Habit-tracker/app.py
+++ Habit-tracker/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+habits = ["Test habit"]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home")
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
+    if request.form == "POST":
+        pass
     return render_template("add_habit.html", title="Habit Tracker - Add Habit")
